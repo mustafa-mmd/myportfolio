5f814db3eefec55ad155962b94f8f0a0f1005400
mustafajam <mustafajamalshabqadar@gmail.com>
2025-02-16 18:39:11 +0500

    update vite file

--- mustafa-ract-portfolio/vite.config.js
+++ mustafa-ract-portfolio/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/myportfolio/" // Add your GitHub repo name here
+  base: "/myportfolio/" // Your GitHub repo name
 })
